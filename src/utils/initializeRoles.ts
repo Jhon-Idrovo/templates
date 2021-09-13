05c82c86a61ae1ff1d79f49c9444f603704025d1
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-09-13 13:35:19 -0500

    d

--- src/utils/initializeRoles.ts
+++ src/utils/initializeRoles.ts
@@ -3,12 +3,14 @@ import Role from "../models/Role";
 export default async function initializeRoles() {
   try {
     const roles = await Role.countDocuments();
-    if (roles > 0) return;
+    console.log(`Detected ${roles} roles`);
+
+    if (roles === 3) return;
 
     const adminRole = new Role({ name: "Admin" });
     const userRole = new Role({ name: "User" });
     const guestRole = new Role({ name: "Guest" });
-    await Promise.all([adminRole.save(), userRole.save()]);
+    await Promise.all([adminRole.save(), userRole.save(), guestRole.save()]);
     console.log("Roles initialized succesfully");
   } catch (error) {
     console.log("An error happened while creating roles", error);
