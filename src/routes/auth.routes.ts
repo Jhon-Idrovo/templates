df3e17d344c4488233cb4caae94fd0aa87fa5da2
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-09-13 20:56:11 -0500

    added routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,7 +2,9 @@
  * endpoints for login and register
  */
 import { Router } from "express";
+import passport from "passport";
 import * as AuthCtlr from "../controllers/auth.controller";
+import { verifyTokenMiddleware } from "../middlewares/verifyToken";
 
 const router = Router();
 router.post("/signin", AuthCtlr.signInHandler); //the access token is send here
@@ -14,7 +16,21 @@ router.post("/create-admin", AuthCtlr.createAdminHandler);
 //scopes https://developers.google.com/identity/protocols/oauth2/scopes?authuser=1#google-sign-in
 router.get("/google", AuthCtlr.handleGoogle);
 //sign in with facebook
-router.get("/facebook", AuthCtlr.handleFacebook);
+router.get("/facebook", passport.authenticate("facebook"));
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", { session: false, scope: "email" }),
+  AuthCtlr.handleFacebook
+);
 //sign in with twitter
-router.get("/twitter", AuthCtlr.handleTwitter);
+router.get("/twitter", passport.authenticate("twitter", { session: false }));
+router.get(
+  "/twitter/callback",
+  passport.authenticate("twitter", { session: false }),
+  AuthCtlr.handleTwitter
+);
+router.use(verifyTokenMiddleware);
+router.post("/signout", AuthCtlr.signOutHandler);
+router.post("/change-password", AuthCtlr.changePassword);
+router.post("/delete-user", AuthCtlr.deleteUser);
 export default router;
